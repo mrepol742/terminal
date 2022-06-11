7fd7cb0a86e66e357fc1256cf8311ad4aa2b8d9a
mrepol742 <mrepol742@gmail.com>
2022-06-12 00:49:50 +0800

    Initial Commit

---
+++ js/commands.js
@@ -0,0 +1,69 @@
+var twitter = "https://www.twitter.com/mrepol742/";
+var password = "edogawaconan";
+var linkedin = "https://www.linkedin.com/in/mrepol742/";
+var instagram = "https://www.instagram.com/melvinjonesrepol/";
+var github = "https://github.com/mrepol742/";
+var email = 'mailto:mrepol742@gmail.com';
+
+whois = [
+  "<br>",
+  "Moshi mos, Melvin Jones Repol desu!",
+  "I'm a Software Engineer with experience in Web Development, SEO, Data Analyst and Computer Troubleshooting. <br>The creator of Webvium. A Self Taught Programer.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/mrepol742' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/mrepol742' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/melvinjonesrepol' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/mrepol742' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Forrest?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">©2022 Melvin Jones Repol | Software Engineer</span><br>',
+
+  "                                   \"\"#    mmmmmm    mm   mmmm <br>" +
+  "mmmmm   m mm   mmm   mmmm    mmm     #        #\"   m\"#  \"   \"#<br>" +
+  "# # #   #\"  \" #\"  #  #\" \"#  #\" \"#    #       m\"   #\" #      m\"<br>" +
+  "# # #   #     #\"\"\"\"  #   #  #   #    #      m\"   #mmm#m   m\"  <br>" +
+  "# # #   #     \"#mm\"  ##m#\"  \"#m#\"    \"mm   m\"        #  m#mmmm<br>" +
+  "                     #                                       <br>",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
